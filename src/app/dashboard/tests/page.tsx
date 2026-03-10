4862079c30fdecc5bf083aa1a3b11a1b69286949
Sohan Rout <sohanrout84@gmail.com>
2026-03-10 10:36:24 +0530

    Style : updated button styling

--- src/app/dashboard/tests/page.tsx
+++ src/app/dashboard/tests/page.tsx
@@ -3,6 +3,10 @@ import Loader from "@/app/components/ui/loader";
 import { useTestStore } from "@/store/testStore";
 import { useEffect } from "react";
 import { useRouter } from "next/navigation";
+import { BsPencilSquare } from "react-icons/bs";
+import { TfiViewListAlt } from "react-icons/tfi";
+import { RiDeleteBin2Fill } from "react-icons/ri";
+import { FaPlus } from "react-icons/fa6";
 
 
 export default function TestsPage() {
@@ -32,29 +36,16 @@ export default function TestsPage() {
           <div
         
             key={test.id}
-            className="bg-white border rounded-xl shadow-md p-6 hover:shadow-lg transition"
+            className="bg-white border rounded-xl shadow-md p-6"
           >
-            <h2 className="text-lg font-semibold mb-2 text-black">
+            <div className="flex justify-between items-center border-b border-neutral-600 py-2">
+              <h2 className="text-xl text-black">
               {test.title}
             </h2>
-
-            <div className="text-sm text-gray-600 space-y-1">
-              <p>📅 Year: {test.year}</p>
-              <p>⏱ Duration: {test.duration_minutes} minutes</p>
-              <p>📝 Total Marks: {test.total_marks}</p>
-            </div>
-
-            <div className="flex gap-3 mt-4">
-              <button 
-                onClick={() => router.push(`/dashboard/tests/${test.id}/questions`)}
-                className="px-3 py-1 text-sm bg-gray-300 text-black rounded hover:bg-gray-400"
-              >
-                View Questions
+            <div className="flex gap-2 justify-center items-center">
+              <button onClick={()=>{router.push(`/dashboard/tests/${test.id}/edit`)}} className="flex items-center justify-center gap-2 bg-neutral-100 border border-neutral-200 shadow-sm text-sm rounded-lg text-black p-2">
+                <BsPencilSquare /> Edit
               </button>
-              <button onClick={()=>{router.push(`/dashboard/tests/${test.id}/edit`)}} className="px-3 py-1 text-sm bg-blue-600 text-white rounded hover:bg-blue-700">
-                Edit
-              </button>
-
               <button
                 onClick={async () => {
                   if (confirm('Are you sure you want to delete this test and all its questions?')) {
@@ -65,14 +56,38 @@ export default function TestsPage() {
                     }
                   }
                 }}
-                className="px-3 py-1 text-sm bg-red-500 text-white rounded hover:bg-red-600"
+                className="flex items-center justify-center gap-2 bg-neutral-100 border border-neutral-200 shadow-sm text-sm rounded-lg text-black p-2"
               >
-                Delete
+                <RiDeleteBin2Fill className="text-lg text-red-500"/>Delete
+              </button>
+            </div>
+            </div>
+
+            <div className="space-y-1 py-4">
+              <span className="text-black text-lg py-2">Current Details:</span>
+              <div className="bg-neutral-50 border border-neutral-200 rounded-lg flex justify-center items-center py-2">
+                <span className="text-black text-sm">Year: {test.year}</span>
+              </div>
+              <div className="bg-neutral-50 border border-neutral-200 rounded-lg flex justify-center items-center py-2">
+                <span className="text-black text-sm">Duration: {test.duration_minutes} minutes</span>
+              </div>
+              <div className="bg-neutral-50 border border-neutral-200 rounded-lg flex justify-center items-center py-2">
+                <span className="text-black text-sm">Total Marks: {test.total_marks}</span>
+              </div>
+            </div>
+
+            <div className="flex flex-col gap-2 border-t border-neutral-600 pt-4">
+              <h1 className="text-lg text-black">Actions:</h1>
+              <button 
+                onClick={() => router.push(`/dashboard/tests/${test.id}/questions`)}
+                className="px-4 py-2 bg-black flex items-center justify-center gap-2 text-white rounded-lg hover:opacity-80 duration-300"
+              >
+                <TfiViewListAlt />View Questions
               </button>
               <button 
                 onClick={() => router.push(`/dashboard/tests/${test.id}/addquestions`)}
-              className="px-3 py-1 text-sm bg-green-500 text-white rounded hover:bg-green-600">
-                Add questions
+              className="px-4 py-2 bg-emerald-300 text-black flex justify-center items-center gap-2 border rounded-lg hover:bg-emerald-500 duration-300">
+                <FaPlus />Add questions
               </button>
             </div>
           </div>
